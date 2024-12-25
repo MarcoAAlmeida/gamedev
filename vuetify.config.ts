8b3614090bbe28ad0e77491d5e9441f3a942ec03
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2024-12-25 16:05:10 -0300

    added support for local default dark/light theme option

--- vuetify.config.ts
+++ vuetify.config.ts
@@ -1,9 +1,11 @@
 // vuetify.config.ts
 import { defineVuetifyConfiguration } from 'vuetify-nuxt-module/custom-configuration'
 
+const isDarkMode = import.meta.client && window.matchMedia('(prefers-color-scheme: dark)').matches
+
 export default defineVuetifyConfiguration({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: isDarkMode ? 'dark' : 'light',
     themes: {
       light: {
         dark: false,
