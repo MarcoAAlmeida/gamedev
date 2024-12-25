c5f5de7d41b41ec02ef277f0f5b90b2881d3300b
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2024-12-25 16:10:05 -0300

    removed import of meta.client (due to error while building in Cloudfare)

--- vuetify.config.ts
+++ vuetify.config.ts
@@ -1,11 +1,9 @@
 // vuetify.config.ts
 import { defineVuetifyConfiguration } from 'vuetify-nuxt-module/custom-configuration'
 
-const isDarkMode = import.meta.client && window.matchMedia('(prefers-color-scheme: dark)').matches
-
 export default defineVuetifyConfiguration({
   theme: {
-    defaultTheme: isDarkMode ? 'dark' : 'light',
+    defaultTheme: 'light',
     themes: {
       light: {
         dark: false,
