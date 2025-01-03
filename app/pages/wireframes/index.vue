14e5f3e631fbc27aba6fa4f9ae8cf45062571e89
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2025-01-03 13:13:03 -0300

    added multiple columns to wireframe index, to check responsive behavior on mobile

--- app/pages/wireframes/index.vue
+++ app/pages/wireframes/index.vue
@@ -3,8 +3,12 @@ const navigationStore = useNavigationStore()
 const {wireframesMenuItems} = storeToRefs(navigationStore)
 
 const gagariumStore = useGagariumStore()
-const {gagarium} = storeToRefs(gagariumStore)
+const { gagarium } = storeToRefs(gagariumStore)
 
+const hand = gagarium.value
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
 
 </script>
 
@@ -18,10 +22,33 @@ const {gagarium} = storeToRefs(gagariumStore)
     <v-main>
       <v-row class="w-100" no-gutters>
         <v-col
-            cols="12"
+            cols="6"
         >
           <v-list lines="one">
             <v-list :items="wireframesMenuItems"/>
+          </v-list>
+        </v-col>
+        <v-col cols="6">
+          <v-list density="compact">
+            <v-list-item
+                v-for="(item, i) in wireframesMenuItems"
+                :key="i"
+                :value="item"
+                :to="item.props?.to"
+                :link="item.props?.link"
+                color="primary"
+            >
+              <template v-slot:prepend>
+                <v-avatar size="x-large">
+                  <nuxt-img
+                      preload
+                      :src="hand[i]"
+                      sizes="64px"/>
+                </v-avatar>
+              </template>
+
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item>
           </v-list>
         </v-col>
         <v-col
@@ -29,7 +56,7 @@ const {gagarium} = storeToRefs(gagariumStore)
         >
           <nuxt-img
             preload
-            v-for="src in gagarium"
+            v-for="src in hand"
             :key="src"
             :src="src"
             sizes="100vw sm:50vw md:400px"/>
